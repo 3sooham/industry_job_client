a173d5554da835eab8f0ff3d924251353c3699d5
Your Name <you@example.com>
2022-02-26 14:05:48 +0900

    Created project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent },
+  // angular에서 **가 와일드카드임
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
